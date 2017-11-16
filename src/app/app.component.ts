3e61b5b8127ed628cbaea30faf50216854b6fe38
Harry <harry@motion1.co.kr>
2017-11-16 12:42:40 +0900

    fixedfocus problem with textbox. deal with cursor arrow up/down in input field

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,83 +2,89 @@ import { Component, OnInit } from '@angular/core';
 import { UISpinDropdownOption } from './ui-spindropdown.component'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'app';
+	title = 'app';
 	ready = false;
-  
-  fsize_options: UISpinDropdownOption = null;
-  
-    ngOnInit() {
-      if (this.ready == false) {
-        this.setup_fsize_options()
-        this.ready = true;
-      }
-    }
-  
-    setup_fsize_options() {
-      this.fsize_options = {
-        items: [
-          { value: 6, name: '6pt' },
-          { value: 8, name: '8pt' },
-          { value: 9, name: '9pt' },
-          { value: 10, name: '10pt' },
-          { value: 11, name: '11pt' },
-          { value: 12, name: '12pt' },
-          { value: 24, name: '24pt' },
-          { value: 18, name: '18pt' },
-          { value: 24, name: '24pt' },
-          { value: 24, name: '24pt' },
-          { value: 30, name: '30pt' },
-          { value: 36, name: '36pt' },
-          { value: 48, name: '48pt' },
-          { value: 60, name: '60pt' },
-          { value: 72, name: '72pt' },
-          { value: 96, name: '96pt' },
-        ],
-        active_index: 3,
-        button_width: 84,
-        list_width: '86px',
-        list_height: '240px',
-        show_list_on_hover: true,
-        input_fn: (str)	=> {
-          str = str.trim();
 
-          let item = null;
+	fsize_options: UISpinDropdownOption = null;
 
-          // 12.32pt 형식
-          const regex = /^([0-9]+(.[0-9]+)?)[ ]*([a-z]*)?/g;
-          let m = regex.exec(str);
-          if (m == null) {
-              alert('잘못된 값입니다.')
-          }
-          else {
-              let val = parseInt(m[1]);
-              let unit = m[3];
-              unit = 'pt'; // 당분강 강제 'pt'로 설정함.
+	ngOnInit() {
+		if (this.ready == false) {
+			this.setup_fsize_options()
+			this.ready = true;
+		}
+	}
 
-              val = Math.max(2, val);
-              val = Math.min(160, val);
-              item = {
-                  name: val + unit,
-                  value: val
-              }
-          }
-          return item;
-        },
-        delta_fn: (value, delta)	=> {
-          let val = parseInt(value) + delta;
-          val = Math.max(2, val);
-          val = Math.min(160, val);
-          let item = {
-              name: val + 'pt',
-              value: val
-          }
-          return item;       
-        },
-      }
-    }  
+	setup_fsize_options() {
+		this.fsize_options = {
+			items: [
+				{ value: 6, name: '6pt' },
+				{ value: 8, name: '8pt' },
+				{ value: 9, name: '9pt' },
+				{ value: 10, name: '10pt' },
+				{ value: 11, name: '11pt' },
+				{ value: 12, name: '12pt' },
+				{ value: 13, name: '13pt' },
+				{ value: 24, name: '24pt' },
+				{ value: 18, name: '18pt' },
+				{ value: 24, name: '24pt' },
+				{ value: 24, name: '24pt' },
+				{ value: 30, name: '30pt' },
+				{ value: 36, name: '36pt' },
+				{ value: 48, name: '48pt' },
+				{ value: 60, name: '60pt' },
+				{ value: 72, name: '72pt' },
+				{ value: 96, name: '96pt' },
+			],
+			active_index: 3,
+			button_width: 84,
+			list_width: '86px',
+			list_height: '240px',
+			show_list_on_hover: true,
+			delta_step: [1, 10],	// 기본 증감은 1, shift누른 상태에서의 증감은 10
+			input_fn: (str) => {
+				str = str.trim();
+
+				let item = null;
+
+				// 12.32pt 형식
+				const regex = /^([0-9]+(.[0-9]+)?)[ ]*([a-z]*)?/g;
+				let m = regex.exec(str);
+				if (m == null) {
+					alert('잘못된 값입니다.')
+				}
+				else {
+					let val = parseInt(m[1]);
+					let unit = m[3];
+					unit = 'pt'; // 당분강 강제 'pt'로 설정함.
+
+					val = Math.max(2, val);
+					val = Math.min(160, val);
+					item = {
+						name: val + unit,
+						value: val
+					}
+				}
+				return item;
+			},
+			delta_fn: (value, delta) => {
+				let val = parseInt(value) + delta;
+				val = Math.max(2, val);
+				val = Math.min(160, val);
+				let item = {
+					name: val + 'pt',
+					value: val
+				}
+				return item;
+			}
+		}
+	}
+
+	foo(event) {
+		console.log(event);
+	}
 }
--- src/app/ui-spindropdown.component.ts
+++ src/app/ui-spindropdown.component.ts
@@ -24,8 +24,18 @@ export interface UISpinDropdownOption {
     list_height?: string;
     large_padding?: boolean;
     show_list_on_hover?: boolean;
+    delta_step: Array<number>;      // [1, 10]인 경우 기본delta는 1, 큰 delta는 10 
     input_fn: (str)=>{name, value}
     delta_fn: (value, delta)=>{name, value}  // value:현재값, delta:적용할 증감값
+}
+export interface UISpinDropdownResult {
+    item: {
+        name: string,
+        value: number,
+    }
+    opt: {
+        focus_control?: string
+    }
 }
 
 @Component({
@@ -39,7 +49,7 @@ export interface UISpinDropdownOption {
                     [attr.active]='!SHOW_LIST'
                     [attr._disabled]='disabled'
                     tabindex='1'>
-                    <input #inputbox class='ui-inputbox' type='text' [value]='getButtonString()' 
+                    <input #inputbox class='ui-inputbox' type='text' [value]='getButtonString()' spellcheck="false"
                         (keydown)='onKeydownInputBox($event)'
                         (focus)='onFocusInputBox()'
                         (blur)='onBlurInputBox()'
@@ -83,7 +93,7 @@ export interface UISpinDropdownOption {
 export class UISpinDropdown implements OnInit {
     @Input('options') options = null;
     @Input('disabled') disabled = false;
-    @Output('select') select = new EventEmitter();
+    @Output('changed') changed = new EventEmitter<UISpinDropdownResult>();
     @ViewChild('inputbox') inputboxEl: ElementRef;
     show_list_case_hover = false;
     tentative_hide = false; // hover시에만 사용됨.
@@ -138,7 +148,6 @@ export class UISpinDropdown implements OnInit {
     focus_blur() {
         // console.log('focus_blur()..');
         this.show_list_case_click = false;
-        
     }
 
     getButtonString() {
@@ -150,16 +159,6 @@ export class UISpinDropdown implements OnInit {
             return '';
     }
 
-    _setValue(item) {
-        // console.log(item);
-        this.show_list_case_hover = false;
-        this.show_list_case_click = false;
-
-        if (this.options.active_index != undefined)
-            this.current_sel = item;
-        this.select.emit(item);
-    }
-    
     onClick(item) {
         console.log('item selected.');
         this._setValue(item)
@@ -179,33 +178,37 @@ export class UISpinDropdown implements OnInit {
         this.inputBoxFocus = false;
         
         let el = this.inputboxEl.nativeElement;
-        this.update(el.value);
+        this.update_by_string_value(el.value);
     }
 
     onKeydownInputBox(event) {
         let el = this.inputboxEl.nativeElement;
-
+        let delta_val = event.shiftKey ? this.options.delta_step[1] : this.options.delta_step[0];
+        
         /* 
             text를 selection(ctrl+a등)하면 focus를 잃게 되고 blur이벤트가 발생하고 거기서 this.update()가 불리는 문제있음.
         */
-
         switch(event.keyCode) {
             case 13: // enter
-                this.update(el.value);
+                this.update_by_string_value(el.value);
+                event.stopPropagation();
+                event.preventDefault();
                 break;
             case 27: //escape
-                this.inputboxEl.nativeElement.value = this.current_sel.name;            
+                this.inputboxEl.nativeElement.value = this.getButtonString();            
+                event.stopPropagation();
+                event.preventDefault();
                 break;
-            // case 38: // arrow up
-            //     this.increase_value(1);
-            //     event.stopPropagation();
-            //     event.preventDefault();   
-            //     break;
-            // case 40: // arrow down
-            //     this.increase_value(-1);
-            //     event.stopPropagation();
-            //     event.preventDefault();   
-            //     break;
+            case 38: // arrow up
+                this.increase_value(delta_val, {focus_control:'dont_focus'});
+                event.stopPropagation();
+                event.preventDefault();
+                break;
+            case 40: // arrow down
+                this.increase_value(-delta_val, {focus_control:'dont_focus'});
+                event.stopPropagation();
+                event.preventDefault();
+                break;
             default:            
                 break;
         }
@@ -214,10 +217,12 @@ export class UISpinDropdown implements OnInit {
         // event.preventDefault();
     }
 
-    update(str) {
+    ////////////////////////////////////////////////////////////////////////////////////
+
+    private update_by_string_value(str) {
         let item = this.options.input_fn(str);
         if (item == null) {
-            this.inputboxEl.nativeElement.value = this.current_sel.name;            
+            this.inputboxEl.nativeElement.value = this.getButtonString();
         }
         else {
             this._setValue(item);
@@ -225,11 +230,34 @@ export class UISpinDropdown implements OnInit {
         }
     }
 
-    increase_value(step) {
+    private increase_value(step, opt?) {
         let cur_item = this.options.input_fn(this.inputboxEl.nativeElement.value);
+        if (cur_item == null)
+            return;
+
         let item = this.options.delta_fn(cur_item.value, step);
 
-        this._setValue(item);
+        // 커서키 up/down으로 값을 변경시 focus가 textbox에 가지 않도록 유지한다.
+        this._setValue(item, opt);
         this.inputboxEl.nativeElement.value = item.name;
     }
+
+    private _setValue(item, opt?) {
+        // console.log(item);
+        this.show_list_case_hover = false;
+        this.show_list_case_click = false;
+
+        if (this.options.active_index != undefined)
+            this.current_sel = this.options.items.find(_item => _item.name == item.name);
+
+        this.options.button_name = item.name;
+        
+        if (item == undefined)
+            console.log('aaa')
+
+        this.changed.emit({
+            item: item,
+            opt: opt,
+        });
+    }    
 }
